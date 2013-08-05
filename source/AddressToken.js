d04846e432131524a6f5763f9155b77fb7a9e092
Till Hofmann <hofmanntill@gmail.com>
2013-08-06 00:49:41 +0200

    use subtokens in address tokens

--- source/AddressToken.js
+++ source/AddressToken.js
@@ -2,13 +2,14 @@ enyo.kind({
   name: "WebLetter.AddressToken",
   kind: "WebLetter.Token",
   style: "margin: 0px; padding: 0px;",
+  topToken: false,
   components: [
     {kind: "onyx.Groupbox", components: [
       {kind: "onyx.GroupboxHeader", name: "descr"},
-      {kind: "WebLetter.TextToken", name: "name", description: "Name", topToken: false},
-      {kind: "WebLetter.TextToken", name: "street", description: "Street", topToken: false},
-      {kind: "WebLetter.TextToken", name: "zip", description: "ZIP Code", topToken: false},
-      {kind: "WebLetter.TextToken", name: "city", description: "City", topToken: false}
+      {kind: "WebLetter.TextToken", name: "name", description: "Name", subkey: "name", subToken: true},
+      {kind: "WebLetter.TextToken", name: "street", description: "Street", subkey: "street", subToken: true},
+      {kind: "WebLetter.TextToken", name: "zip", description: "ZIP Code", subkey: "zip", subToken: true},
+      {kind: "WebLetter.TextToken", name: "city", description: "City", subkey: "city", subToken: true}
     ]}],
   getInput: function() {
     var res = this.$.name.getValue()
