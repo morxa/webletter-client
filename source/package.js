874433a89632ea10866f68655b7b2def1e542c79
Till Hofmann <hofmanntill@gmail.com>
2013-09-16 19:25:17 +0200

    LocalStorage: add LocalStorage kind

--- source/package.js
+++ source/package.js
@@ -2,6 +2,7 @@ enyo.depends(
 	"$lib/layout",
 	"$lib/onyx",	// To theme Onyx using Theme.less, change this line to $lib/onyx/source,
 	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
+  "LocalStorage.js",
   "Token.js",
   "BooleanToken.js",
   "TextToken.js",
