6dc39b4efff6ff0799a3ae6e41322c0723ed6749
Till Hofmann <hofmanntill@gmail.com>
2013-11-20 12:10:42 +0100

    BooleanToken: use opt-token functionality
    a BooleanToken does the same thing as an optional token, simply use the
    optional token functionality to avoid duplication

--- source/BooleanToken.js
+++ source/BooleanToken.js
@@ -1,16 +1,12 @@
 enyo.kind({
   name: "WebLetter.BooleanToken",
   kind: "WebLetter.Token",
+  isOptional: true,
   components: [
-    {tag: "span", name: "descr", classes: "nice-padding"},
-    {kind: "onyx.ToggleButton", name: "input", onContent: "yes", offContent: "no"}
+    {tag: "span", name: "descr", classes: "nice-padding"}
+    //{kind: "onyx.ToggleButton", name: "input", onContent: "yes", offContent: "no"}
   ],
   getInput: function() {
-    if (this.$.input.getValue()) {
-      return "true";
-    }
-    else {
-      return "false";
-    }
+    return this.$.toggle.getValue();
   }
 });
