7ed660bd705f6ec6fd974b3035ffb2d96e5926df
Till Hofmann <hofmanntill@gmail.com>
2013-12-04 21:43:54 +0100

    TextToken: add default width (100%)

--- source/TextToken.js
+++ source/TextToken.js
@@ -1,6 +1,9 @@
 enyo.kind({
   name: "WebLetter.TextToken",
   kind: "WebLetter.Token",
+  published: {
+    inputWidth: "100%"
+  },
   components: [
     {kind: "onyx.InputDecorator", components: [
       {name: "descr", classes: "pad-right"},
@@ -10,6 +13,10 @@ enyo.kind({
   ],
   create: function() {
     this.inherited(arguments);
+    this.$.input.applyStyle("width", this.getInputWidth());
+  },
+  inputWidthChanged: function(oldValue) {
+    this.$.input.applyStyle("width", this.getInputWidth());
   },
   getValue: function() {
     return this.$.input.getValue();
